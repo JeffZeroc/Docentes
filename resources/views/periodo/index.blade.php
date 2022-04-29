a355a4beb142a9b1abd8daff6fa9332c09ec0b08
JeffZeroc <jefferbonevalencia@gmail.com>
2022-04-28 19:56:11 -0500

    Titulo_modificados

--- resources/views/periodo/index.blade.php
+++ resources/views/periodo/index.blade.php
@@ -11,7 +11,7 @@
                         <div style="display: flex; justify-content: space-between; align-items: center;">
 
                             <span id="card_title">
-                                {{ __('Periodo') }}
+                                {{ __('Periodos') }}
                             </span>
 
                              <div class="float-right">
