a475610dbf31b5ff8e499b7b29ab3170125c20e2
JeffZeroc <jefferbonevalencia@gmail.com>
2022-05-22 08:47:31 -0500

    logo_inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -10,14 +10,7 @@
                 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                       <div class="carousel-item active">
-                        <img src="https://estudiante.utelvt.edu.ec/assets/matricula2018-9cd5d2e97e633297b9a94f517a21dc047ac40ee0f01fcc0e39c909605ae24b50.jpg" class="d-block w-100" alt="..." width="300" height="300">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="https://estudiante.utelvt.edu.ec/assets/matricula2018-9cd5d2e97e633297b9a94f517a21dc047ac40ee0f01fcc0e39c909605ae24b50.jpg" class="d-block w-100" alt="..." width="300" height="300">
-                      </div>
-                      <div class="carousel-item">
-                        <img src="https://estudiante.utelvt.edu.ec/assets/matricula2018-9cd5d2e97e633297b9a94f517a21dc047ac40ee0f01fcc0e39c909605ae24b50.jpg" class="d-block w-100" alt="..." width="300" height="300">
-                      </div> 
+                        <img src="{{ asset('img/utelvt_inicio.png')}}" class="d-block w-100" alt="..." >
                     </div>
                   </div>
             </div>
