a1f4f9d030e5f7eb75e5bc77d4e8cb0f96d749f7
JeffZeroc <jefferbonevalencia@gmail.com>
2022-05-23 11:08:52 -0500

    fin1

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,25 +1,27 @@
 @extends('layouts.app')
 
-@section('title','Inicio')
+@section('title', 'Inicio')
 
 
 
 @section('content')
-    
-            <div class="row justify-content-center">
-                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img src="{{ asset('img/utelvt_inicio.png')}}" class="d-block w-100" alt="..." >
+
+    <div class="row justify-content-center">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="container" style="width: 65%;">
+                        <img src="{{ asset('img/SGRD.png') }}" height=""  class="d-block w-100" alt="...">
                     </div>
-                  </div>
+                </div>
             </div>
-@endsection
+        </div>
+    @endsection
 
 
-@section('script')
-    <!-- Page level custom scripts -->
-    
+    @section('script')
+        <!-- Page level custom scripts -->
 
-    
-@endsection
+
+
+    @endsection
