12f138051f1e5beb0247b8f98df0c8bbc3bf8c32
JeffZeroc <jefferbonevalencia@gmail.com>
2022-05-25 11:59:04 -0500

    as

--- resources/views/layouts/app_admin.blade.php
+++ resources/views/layouts/app_admin.blade.php
@@ -40,7 +40,37 @@
 <body id="body-pd">
     <header class="header static" id="header">
         <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-        @include('partials.topbar')
+        <div>
+            @guest
+    @if (Route::has('login'))
+        <li class="nav">
+            <a class="nav-link" href="{{ route('login') }}"> {{ __('Login') }}</a>
+        </li>
+    @endif
+@else
+    <!-- Large button groups (default and split) -->
+    <div class="btn-group">
+        <a class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            <span class="small">{{ Auth::user()->name }}</span>
+            <img class="img-profile rounded-circle" src="/img/docente.png" height="25" width="25">
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start" aria-labelledby="dropdownMenuButton1"
+            data-bs-display="static" aria-expanded="false">
+            <li><a class="dropdown-item" href="/home/users"><i class="bx bx-user-circle"></i>
+                    <span class="small">Usuarios</span></a></li>
+
+            <div class="dropdown-divider"></div>
+
+            <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal" href="#">
+                    <i class="bx bx-log-in"></i>
+                    <span class="small">Cerrar Sesión</span>
+                </a></li>
+        </ul>
+    </div>
+
+@endguest
+        </div>
     </header>
     <div class="l-navbar" id="nav-bar">
         <nav class="nav">
--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -1,48 +1,39 @@
+<!-- Sidebar Toggle (Topbar) -->
 
 
-    <!-- Sidebar Toggle (Topbar) -->
-    
-    
-    <!-- Topbar Search -->
-    
-    <!-- Topbar Navbar -->
-    
-        <div class="topbar-divider d-none d-sm-block"></div>
+<!-- Topbar Search -->
 
-        {{-- User Informatio --}}
-            @guest
-                @if (Route::has('login'))
-                    <li class="nav">
-                        <a class="nav-link" href="{{ route('login') }}"> {{ __('Login') }}</a>
-                    </li>
-                @endif
-                {{-- 
-                @if (Route::has('register'))
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('register')}}">{{ __('Register')}}</a>
-                </li>
-                @endif --}}
+<!-- Topbar Navbar -->
 
-                @else
-                <!-- Large button groups (default and split) -->
-                <div class="btn-group">
-                    <a class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <span class="small">{{ Auth::user()->name }}</span>
-                        <img class="img-profile rounded-circle" src="/img/docente.png" height="25" width="25">
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start" aria-labelledby="dropdownMenuButton1" data-bs-display="static" aria-expanded="false">
-                        <li><a class="dropdown-item" href="/home/users"><i class="bx bx-user-circle"></i>
-                            <span class="small">Usuarios</span></a></li>
+<div class="topbar-divider d-none d-sm-block"></div>
 
-                        <div class="dropdown-divider"></div>
+{{-- User Informatio --}}
+@guest
+    @if (Route::has('login'))
+        <li class="nav">
+            <a class="nav-link" href="{{ route('login') }}"> {{ __('Login') }}</a>
+        </li>
+    @endif
+@else
+    <!-- Large button groups (default and split) -->
+    <div class="btn-group">
+        <a class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+            aria-expanded="false">
+            <span class="small">{{ Auth::user()->name }}</span>
+            <img class="img-profile rounded-circle" src="/img/docente.png" height="25" width="25">
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-lg-start" aria-labelledby="dropdownMenuButton1"
+            data-bs-display="static" aria-expanded="false">
+            <li><a class="dropdown-item" href="/home/users"><i class="bx bx-user-circle"></i>
+                    <span class="small">Usuarios</span></a></li>
 
-                        <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal" href="#">
-                            <i class="bx bx-log-in"></i>
-                            <span class="small">Cerrar Sesión</span>
-                        </a></li>
-                    </ul>
-                </div>
-                       
-            @endguest
+            <div class="dropdown-divider"></div>
 
-    
+            <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exampleModal" href="#">
+                    <i class="bx bx-log-in"></i>
+                    <span class="small">Cerrar Sesión</span>
+                </a></li>
+        </ul>
+    </div>
+
+@endguest
